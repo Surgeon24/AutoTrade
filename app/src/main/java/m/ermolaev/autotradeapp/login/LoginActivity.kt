68f5a2db709ccae5f6972192e9e48853085c26d7
Mikhail <ermol24@icloud.com>
2024-02-29 00:26:41 +0100

    added pinkman library

--- app/src/main/java/m/ermolaev/autotradeapp/login/LoginActivity.kt
+++ app/src/main/java/m/ermolaev/autotradeapp/login/LoginActivity.kt
@@ -4,14 +4,17 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.redmadrobot.pinkman.Pinkman
 import m.ermolaev.autotradeapp.R
 
 
 
 class LoginActivity : AppCompatActivity() {
-
+    val pinkman = Pinkman(application.applicationContext)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -24,6 +27,26 @@ class LoginActivity : AppCompatActivity() {
             replace(R.id.container, loginFragment)
             addToBackStack(null)
             commit()
+        }
+    }
+
+    class CreatePinViewModel(private val pinkman: Pinkman) : ViewModel() {
+
+        val pinIsCreated = MutableLiveData<Boolean>()
+
+        fun createPin(pin: String) {
+            pinkman.createPin(pin)
+
+            pinIsCreated.postValue(true)
+        }
+    }
+
+    class InputPinViewModel(private val pinkman: Pinkman) : ViewModel() {
+
+        val pinIsValid = MutableLiveData<Boolean>()
+
+        fun validatePin(pin: String) {
+            pinIsValid.value = pinkman.isValidPin(pin)
         }
     }
 
